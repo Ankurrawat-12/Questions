0b71c3cbb9291dd815ce1a27fcd2dea9eee415ee
Ankur Rawat <ankurrawat620@gmail.com>
2022-10-07 01:04:13 +0530

    completed roman to integer code and added a hello world file

--- roman_to_integer.cpp
+++ roman_to_integer.cpp
@@ -20,6 +20,75 @@ int main(){
     cin>>roman;
     int number = 0;
     for(int i = 0; i < roman.length(); i++){
-        if()
+        if(roman[i] == 'I'){
+            if((roman[i+1] == 'X') || (roman[i+1] =='V')){
+                number += 0;
+            }
+            else{
+                number += 1;
+            }
+        }
+        if(roman[i] == 'V'){
+            if(roman[i-1] == 'I'){
+                number += 4;
+            }
+            else{
+                number += 5;
+            }
+        }
+        if(roman[i] == 'X'){
+            if(roman[i-1] == 'I'){
+                number += 9;
+            }
+            else{
+                if((roman[i+1] == 'L') || (roman[i+1] == 'C')){
+                    number += 0;
+                }
+                else{
+                    number += 10;
+                }
+            }
+        }
+        if(roman[i] == 'L'){
+            if(roman[i-1] == 'X'){
+                number += 40;
+            }
+            else{
+                number += 50;
+            }
+        }
+        if(roman[i] == 'C'){
+            if(roman[i-1] == 'X'){
+                number += 90;
+            }
+            else{
+                if((roman[i+1] == 'M') || (roman[i+1] == 'D')){
+                    number += 0;
+                }
+                else{
+                    number += 100;
+                }
+            }
+        }
+        if(roman[i] == 'D'){
+            if(roman[i-1] == 'C'){
+                number += 400;
+            }
+            else{
+                number += 500;
+            }
+        }
+        if(roman[i] == 'M'){
+            if(roman[i-1] == 'C'){
+                number += 900;
+            }
+            else{
+                number += 1000;
+            }
+        }
     }
+    cout<<number;
+    return 0;
 }
+
+// done
