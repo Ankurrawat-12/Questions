56dbaaef5cb0be39ddcea10e32de9956a8cd5c39
Ankur Rawat <ankurrawat620@gmail.com>
2022-10-09 01:13:06 +0530

    added first_unique_character_in_a_string problem

---
+++ first_unique_character_in_a_string.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main(){
+    string x_string;
+    cin>>x_string;
+
+    for(int i = 0; i < x_string.size(); i++){
+        int count = 0;
+        for(int j = 0; j < x_string.size(); j++){
+            if((x_string[i] == x_string[j]) && (i != j)){
+                count ++;
+                break;
+            }
+        }
+        if(count == 0){
+            cout<<i;
+            return 0;
+        }
+    }
+    cout<<-1;
+    return 0;
+}
