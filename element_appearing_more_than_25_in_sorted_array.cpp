90f921fbdb4292d4faae0bb51c0ee8436aa3de3c
Ankur Rawat <ankurrawat620@gmail.com>
2022-10-10 20:46:41 +0530

    added element_appearing_moe_then_25percent_in_sorted_arrat

---
+++ element_appearing_more_than_25_in_sorted_array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int length;
+    cin>>length;
+
+    int sorted_array[length];
+    for(int i = 0; i < length; i++){
+        cin>>sorted_array[i];
+    }
+    i
+}
