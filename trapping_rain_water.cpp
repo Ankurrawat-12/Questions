6bfa46fa181df25bfc4be49aa2ed2bccd22d1e85
Ankur Rawat <ankurrawat620@gmail.com>
2022-10-21 23:46:15 +0530

    trapping rain water ( TC :- theta(n))

--- trapping_rain_water.cpp
+++ trapping_rain_water.cpp
@@ -14,17 +14,32 @@ int main()
 
     int waterStored = 0;
 
-    for(int i = 1; i < noOfBars -1; i++){
-        int leftMax = bars[i];
-        for(int j = 0; j < i; j++){
-            leftMax = max(leftMax, bars[j]);
-        }
-        int rightMax = bars[i];
-        for(int j = i +1; j< noOfBars; j++){
-            rightMax = max(rightMax, bars[j]);
-        }
-        waterStored += (min(leftMax, rightMax) - bars[i]);
+    // for(int i = 1; i < noOfBars -1; i++){
+    //     int leftMax = bars[i];
+    //     for(int j = 0; j < i; j++){
+    //         leftMax = max(leftMax, bars[j]);
+    //     }
+    //     int rightMax = bars[i];
+    //     for(int j = i +1; j< noOfBars; j++){
+    //         rightMax = max(rightMax, bars[j]);
+    //     }
+    //     waterStored += (min(leftMax, rightMax) - bars[i]);
+    // }  // time complexity = theta(n^2)
+
+
+    int leftMax[noOfBars], rightMax[noOfBars];
+    leftMax[0] = bars[0];
+    for(int i = 1; i < noOfBars; i++){
+        leftMax[i] = max(leftMax[i-1], bars[i]);
     }
+
+    rightMax[noOfBars-1] = bars[noOfBars-1];
+    for(int i = noOfBars - 2; i >= 0; i--){
+        rightMax[i] = max(rightMax[ i + 1], bars[i]);
+    }
+    for(int i = 1; i < noOfBars - 1; i++){
+        waterStored += (min(rightMax[i], leftMax[i]) - bars[i]);
+    }  // time complexity = theta(n)
 
     cout<<waterStored;
     return 0;
