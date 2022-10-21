5a731208b024be5de45ede4ab5f47e39406fcf38
Ankur Rawat <ankurrawat620@gmail.com>
2022-10-21 19:47:06 +0530

    Trapping rain water

--- trapping_rain_water.cpp
+++ trapping_rain_water.cpp
@@ -4,10 +4,31 @@ using namespace std;
 
 int main()
 {
-    int n;
-    int height[n];
-    
+    int noOfBars;
+    cin>>noOfBars;
+
+    int bars[noOfBars];
+    for(int barPosition = 0; barPosition < noOfBars; barPosition++){
+        cin>>bars[barPosition];
+    }
+
+    int waterStored = 0;
+
+    for(int i = 1; i < noOfBars -1; i++){
+        int leftMax = bars[i];
+        for(int j = 0; j < i; j++){
+            leftMax = max(leftMax, bars[j]);
+        }
+        int rightMax = bars[i];
+        for(int j = i +1; j< noOfBars; j++){
+            rightMax = max(rightMax, bars[j]);
+        }
+        waterStored += (min(leftMax, rightMax) - bars[i]);
+    }
+
+    cout<<waterStored;
+    return 0;
 }
 
-// not done
+// done
 // https://leetcode.com/problems/trapping-rain-water/
