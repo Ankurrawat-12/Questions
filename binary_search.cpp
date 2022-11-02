89d6f09ccd2b14f075a1474a9b83f26848a9762c
Ankur Rawat <ankurrawat620@gmail.com>
2022-11-02 15:01:18 +0530

    binary search

---
+++ binary_search.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+
+using namespace std;
+
+int binarySearch(int arr[], int sizeOfArray, int toFind)
+{
+    // for sorted array
+    int low = 0;
+    int high = sizeOfArray - 1;
+    int mid = (low + high) / 2;
+    while (low <= high)
+    {
+        if (arr[mid] == toFind)
+        {
+            return mid;
+        }
+        else if (arr[mid] > toFind)
+        {
+            high = mid - 1;
+            mid = (low + high) / 2;
+        }
+        else
+        {
+            low = mid + 1;
+            mid = (low + high) / 2;
+        }
+    }
+    return -1;
+}
+
+int linearSearch(int arr[], int sizeOfArray, int toFind)
+{
+    // for non sorted array
+    for (int i = 0; i < sizeOfArray; i++)
+    {
+        if (toFind == arr[i])
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int recursiveBinarySearch(int arr[], int toFind, int low, int high, int mid)
+{
+    // for sorted array
+    if (low > high)
+    {
+        return -1;
+    }
+    else if (arr[mid] == toFind)
+    {
+        return mid;
+    }
+    else if (arr[mid] > toFind)
+    {
+        return recursiveBinarySearch(arr, toFind, low, (mid - 1), (low + high) / 2);
+    }
+    else
+    {
+        return recursiveBinarySearch(arr, toFind, (mid + 1), high, (low + high) / 2);
+    }
+}
+
+int indexOfFirstOccurrence(int arr[], int toFind, int low, int high, int mid)
+{
+    // for sorted array
+    if (low > high)
+    {
+        return -1;
+    }
+    else if (arr[mid] == toFind)
+    {
+        if (arr[mid] != arr[mid - 1] || mid == 0)
+        {
+            return mid;
+        }
+        else
+        {
+            return indexOfFirstOccurrence(arr, toFind, low, high, mid - 1);
+        }
+    }
+    else if (arr[mid] > toFind)
+    {
+        return indexOfFirstOccurrence(arr, toFind, low, (mid - 1), (low + high) / 2);
+    }
+    else
+    {
+        return indexOfFirstOccurrence(arr, toFind, (mid + 1), high, (low + high) / 2);
+    }
+}
+
+int main()
+{
+    int sizeOfArray;
+    cin >> sizeOfArray;
+    int arr[sizeOfArray];
+    for (int i = 0; i < sizeOfArray; i++)
+    {
+        cin >> arr[i];
+    }
+    int toFind;
+    cin >> toFind;
+
+    // int index = binarySearch(arr, sizeOfArray, toFind);
+    // int index = linearSearch(arr, sizeOfArray, toFind);
+    // int index = recursiveBinarySearch(arr, toFind, 0, sizeOfArray-1, (sizeOfArray-1)/2);
+    int index = indexOfFirstOccurrence(arr, toFind, 0, sizeOfArray - 1, (sizeOfArray - 1) / 2);
+    cout << index;
+}
